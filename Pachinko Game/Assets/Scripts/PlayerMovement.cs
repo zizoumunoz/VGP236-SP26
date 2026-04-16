947a0257b75eb540ec1c1684cff69f4999dfbf78
Zinadine Muñoz Luna <zmunoz@outlook.es>
2026-04-16 11:09:12 -0700

    Add script for player movement

---
+++ Pachinko Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    public float movementSpeed = 5f;
+    public Vector2 velocity;
+
+    void Update()
+    {
+        float x = 0;
+
+        if (Keyboard.current.aKey.isPressed )
+        {
+            x = -1;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            x = 1;
+        }
+
+        velocity = new Vector2(x * movementSpeed, 0);
+        transform.position += (Vector3)(velocity * Time.deltaTime); // REMEMBER TO USE DELTA TIME SO MOVEMENT ISNT FRAME TIED!!
+
+    }
+}
