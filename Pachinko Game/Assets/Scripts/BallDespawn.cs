bad13e9eb8229571de40a5cae211792a12bd125b
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-04-20 00:54:15 -0700

    Add dynamic pegs

--- Pachinko Game/Assets/Scripts/BallDespawn.cs
+++ Pachinko Game/Assets/Scripts/BallDespawn.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class BallDespawn : MonoBehaviour
 {
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Goal"))
