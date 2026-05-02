48836d6aa785c3d35772cc620a28b852fddf4774
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-05-01 18:24:36 -0700

    Add idle and walking animations

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    // animating stuff
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+
     // because its a platformer, not vertical movement, just jump movement
     [SerializeField] float _movementSpeed;
     [SerializeField] float _jumpSpeed;
@@ -41,6 +46,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _targetMoveSpeed = _moveAction.ReadValue<Vector2>().x * _movementSpeed;
+        // set the animators speed param so blend trees can use it
+        _animator.SetFloat("Speed", Mathf.Abs(_targetMoveSpeed));
+        if (_targetMoveSpeed > 0.1f)
+        {
+            _spriteRenderer.flipX = false; // don't flip whatever sprite is playing when the speed is going right
+        }
+        else if (_targetMoveSpeed < -0.1f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+        Debug.Log(_targetMoveSpeed);
+
     }
 
     // Like Update() but not frame based, for physics
