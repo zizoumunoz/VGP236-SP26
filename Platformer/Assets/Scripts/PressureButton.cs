9b93ecb537ebf388b532b2689e725aa677220136
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-05-04 17:58:48 -0700

    Add button Prefab

---
+++ Platformer/Assets/Scripts/PressureButton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PressureButton : MonoBehaviour
+{
+    [SerializeField] private Animator animator;   // for button anim
+    [SerializeField] private GameObject targetObject; // object that will be affected
+
+    private int playersOnButton = 0;
+    [SerializeField] private int playersNeeded = 1;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playersOnButton++;
+
+            if (playersOnButton >= playersNeeded)
+            {
+                if (animator != null)
+                    animator.SetBool("Pressed", true);
+
+                targetObject.SendMessage("Activate", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playersOnButton--;
+
+            if (playersOnButton == 0)
+            {
+                if (animator!= null)
+                {
+                    animator.SetBool("Pressed", false);
+                }
+
+                targetObject.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+}
