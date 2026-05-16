b1389a4a3ddaa07a19e01fdf3ddafbdb46af2901
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-05-16 15:26:32 -0700

    Added player movement script

--- Fall Guys Study/Assets/Scripts/PlayerController.cs
+++ Fall Guys Study/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Rigidbody _rigidBody;
+    private Vector2 _moveInput;
+
+    void Awake()
     {
-        
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue value)
     {
-        
+        _moveInput = value.Get<Vector2>();
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(_moveInput.x, 0f, _moveInput.y);
+
+        Vector3 targetVelocity = move * _moveSpeed;
+        targetVelocity.y = _rigidBody.linearVelocity.y;
+
+        _rigidBody.linearVelocity = targetVelocity;
+
+        Vector3 lookDirection = move;
+        if (lookDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+            _rigidBody.MoveRotation(targetRotation);
+        }
+    }
+
 }
